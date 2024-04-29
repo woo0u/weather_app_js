8563bbbdba82632dbd47c410571d28b38b88d7d1
woo0u <wooj1116@gmail.com>
2024-04-29 12:52:51 +0900

    0429

---
+++ js/app.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * @param {NodeList} elmts selected elements from HTML to add events
+ * @param {string} event event type e.g. 'click', 'mouseenter'...
+ * @param {Function} callback callback function to be executed when event is triggered
+ */
+export function eventOnElmts(elmts, event, callback) {
+  for (const elmt of elmts) {
+    elmt.addEventListener(event, callback);
+  }
+}
+
+const a = document.querySelector('.current-weather .heading');
+
+a.addEventListener('click', () => {
+  a.classList.toggle('on');
+});
